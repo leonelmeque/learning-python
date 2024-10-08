858eee831c0572b411e8e96b60236e2018c2c871
Leonel Meque <leomeque@gmail.com>
2024-10-08 08:19:42 +0200

    tuples and sets section

---
+++ tuples_and_sets/main.py
@@ -0,0 +1,13 @@
+nums = (1, 2, 3)
+print(1 in nums)
+print(nums[2]) # accessing value at index
+print(nums.count) # counting number of items
+print(nums.index(1)) # index value
+
+# tuple inside a tuple
+tuple_dub = (1,2,3,(4,5))
+print(tuple_dub)
+
+s = {1, 2, 3, 4}
+
+print(2 in s)
