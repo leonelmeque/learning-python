e0b1d590ed276e6a9835ef31ec9220a0ff1cbb07
Leonel Meque <leomeque@gmail.com>
2024-10-10 08:31:07 +0200

    tuples and sets section

--- tuples_and_sets/main.py
+++ tuples_and_sets/main.py
@@ -8,6 +8,26 @@ print(nums.index(1)) # index value
 tuple_dub = (1,2,3,(4,5))
 print(tuple_dub)
 
-s = {1, 2, 3, 4}
+cities = ['New York', 'New York', 'Los Angeles', 'San Francisco', 'San Francisco', 'San Francisco']
+s = set(cities)
+print(set(cities))
 
 print(2 in s)
+
+for city in s:
+    print(city)
+
+s.add('Chicago')
+print(s)
+
+s.discard(1)
+print(s)
+
+# Suppose I teach two classes:
+math_student = {'Matthew', 'Helen', 'Prashant', 'James', 'Aparna'}
+biology_students = {'Jane', 'Matthew', 'Charlotte', 'Mesut', 'Oliver', 'James'}
+
+print(math_student | biology_students)
+print(math_student & biology_students)
+
+
